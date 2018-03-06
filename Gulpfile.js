3ef7df4ec96e5f645b924ebf35b3b60dd763e3e9
ConnorChristensen <chriconn@oregonstate.edu>
2018-03-06 13:16:03 -0800

    addition of source code

---
+++ Gulpfile.js
@@ -0,0 +1,99 @@
+'use strict';
+var gulp   = require('gulp');
+// automatically load gulp plugins into the object plugin
+var plugin = require("gulp-load-plugins")({
+   pattern: ['gulp-*', 'gulp.*'],
+   replaceString: /\bgulp[\-.]/
+});
+var browserSync = require('browser-sync').create();
+var config      = require('config.json')('./config.json');
+var root        = config.root + "/";
+var destination = config.end + "/";
+var paths = {
+   start: {
+      html: root + config.folders.html + "/*.html",
+      css:  root + config.folders.css  + "/*.styl",
+      js:   root + config.folders.js   + "/*.js"
+   }
+}
+///////////
+// Files //
+///////////
+// move all the html files to the destination folder
+gulp.task('html', function() {
+   return gulp.src(paths.start.html)  // get our html files
+      .pipe(plugin.flatten())         // flatten the folder hierarchy
+      .pipe(gulp.dest(destination));  // send them over to the destination folder
+});
+
+// get all js files and put them in the destination folder
+gulp.task('js', function() {
+   return gulp.src(paths.start.js)    //get our js files
+      .pipe(gulp.dest(destination));  //move them to our destination folder
+});
+
+// compile our stylus files and send them straight to dist
+gulp.task('css', function() {
+   return gulp.src(paths.start.css)   //get our css files
+      .pipe(plugin.flatten())
+      .pipe(plugin.stylus())          //compile them
+      .pipe(gulp.dest(destination));  //put them in a temp folder
+});
+
+gulp.task('images', function() {
+   // get our images folder and send them over to an images folder in destination
+   return gulp.src(root + config.folders.images + '/**/*')
+      .pipe(plugin.print())
+      .pipe(gulp.dest(destination + 'images'));
+});
+
+
+//////////////
+// Cleaning //
+//////////////
+// clean the destination folder and do it blindly to speed it up
+gulp.task('clean', function() {
+   return gulp.src(destination, {read: false}).pipe(plugin.clean());
+});
+
+
+///////////
+// Watch //
+///////////
+gulp.task('watch', function() {
+   gulp.watch(root + '**/*.styl', gulp.series('css'));
+   gulp.watch(root + '**/*.html', gulp.series('html'));
+   gulp.watch(root + '**/*.js',   gulp.series('js'));
+   gulp.watch(root + config.folders.images + '**/*', gulp.series("images"));
+});
+
+// serve up the page on a browser
+gulp.task('serve', function() {
+    browserSync.init({
+        server: {
+            baseDir: destination
+        },
+        browser: "google chrome"
+    });
+    gulp.watch(root + '**/*').on('change', browserSync.reload);
+});
+
+////////////////
+// Main Calls //
+////////////////
+//simply builds the dist folder. Good for testing the build
+//process without serving the site
+gulp.task('build', gulp.parallel(
+  'html', 'css', 'js', 'images'
+));
+
+//build, format the socket.io url to localhost and serve the site
+gulp.task('local', gulp.series(
+   'build',
+   gulp.parallel('watch', 'serve')
+));
+
+gulp.task('rebuild', gulp.series('clean', 'build'));
+
+//build for local by default
+gulp.task('default', gulp.parallel('local'));
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# resume
+# Resume
 An online resume
+
+
+## Installation Instructions
+1. clone the repository
+1. yarn install
+
+## Use Instructions
+This project makes use of Gulp v4.0 which is still under development. It has really nice features built in that make build pipelines quick and efficient.
+
+To see a list of all available tasks, type `gulp ---tasks`.
+
+The ones that you should be calling are probably:
+* `gulp` - which builds a version of the site designed to work locally
+* `gulp build` - builds without serving up the site
+* `gulp clean` - cleans out the files that Gulp builds
